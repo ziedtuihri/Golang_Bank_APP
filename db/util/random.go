2f8bc5f3993000c5f1fbd8d74170f76fb4bf7d7e
zth <zied.hack@gmail.com>
2023-03-31 12:52:59 +0100

    some random changements

--- db/util/random.go
+++ db/util/random.go
@@ -1,25 +1,24 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 	"time"
 )
 
-const (
-	alphabet = "abcdefghijklmnopqrstuvwxyz"
-)
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-// Random generates a random integer between min and max
+// RandomInt generates a random integer between min and max
 func RandomInt(min, max int64) int64 {
 	return min + rand.Int63n(max-min+1)
 }
 
-// RandomString generate a randoms string of length n
+// RandomString generates a random string of length n
 func RandomString(n int) string {
 	var sb strings.Builder
 	k := len(alphabet)
@@ -32,17 +31,24 @@ func RandomString(n int) string {
 	return sb.String()
 }
 
-// RandomOwner generates a random account of money
+// RandomOwner generates a random owner name
 func RandomOwner() string {
 	return RandomString(6)
 }
 
+// RandomMoney generates a random amount of money
 func RandomMoney() int64 {
 	return RandomInt(0, 1000)
 }
 
+// RandomCurrency generates a random currency code
 func RandomCurrency() string {
-	currencies := []string{"EUR", "USD", "CAD"}
+	currencies := []string{"USD", "EUR", "CAD"}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
+}
+
+// RandomEmail generates a random email
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(6))
 }
