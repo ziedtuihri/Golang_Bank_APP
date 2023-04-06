6edd48cf7ec6e5483819256531bdfcc6d12f14fc
zth <zied.hack@gmail.com>
2023-04-06 22:40:08 +0100

    entry sqlc sql

---
+++ db/query/entry.sql
@@ -0,0 +1,17 @@
+-- name: CreateEntry :one
+INSERT INTO entries (
+  account_id,
+  amount
+) VALUES (
+  $1, $2
+)
+RETURNING *;
+
+-- name: GetEntry :one
+SELECT * FROM entries
+WHERE id = $1 LIMIT 1;
+
+
+-- name: ListEntrys :many
+SELECT * FROM entries
+ORDER BY name;
